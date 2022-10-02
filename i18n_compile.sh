ea092934001bd4971843e8659e4810049102fa41
Noboru <somedevfox@gmail.com>
2022-10-02 07:14:27 +0300

    Create Russian Language PO

---
+++ i18n_compile.sh
@@ -0,0 +1,18 @@
+# Copyright (C) 2022 Egor Poleshko
+# 
+# This file is part of Discord-to-Telegram Bridge Bot.
+# 
+# Discord-to-Telegram Bridge Bot is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# Discord-to-Telegram Bridge Bot is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with Discord-to-Telegram Bridge Bot.  If not, see <http://www.gnu.org/licenses/>.
+
+msgfmt -o locales/ru.mo locales/ru.po
